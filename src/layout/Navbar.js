9dad2765deac62721b0b545e89db602d780e0a64
Alain Mignot <alainmignot.tech@gmail.com>
2025-12-02 23:46:20 -0700

    Place action buttons, and config the routing using react-router-dom

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/home">
+          <a className="navbar-brand" href="#">
             User Management App
           </a>
           <button
@@ -19,7 +20,9 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <button className="btn btn-outline-light">Add User</button>
+          <Link className="btn btn-outline-light" to="/adduser">
+            Add User
+          </Link>
         </div>
       </nav>
     </div>
