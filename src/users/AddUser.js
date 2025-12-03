761b9eaad6949d6bacc7dcb0792b0dbd54d3d094
Alain Mignot <alainmignot.tech@gmail.com>
2025-12-03 00:59:31 -0700

    Support posting data into database using Axios POST

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddUser() {
+  let navigate = useNavigate();
+
   const [user, setUser] = useState({
     name: "",
     username: "",
@@ -13,56 +17,64 @@ export default function AddUser() {
     setUser({ ...user, [event.target.name]: event.target.value });
   };
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post("http://localhost:8080/users", user);
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Register User</h2>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your name"
-              name="name"
-              value={name}
-              onChange={(event) => onInputChange(event)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Username" className="form-label">
-              Username
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your username"
-              name="username"
-              value={username}
-              onChange={(event) => onInputChange(event)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Email" className="form-label">
-              E-mail
-            </label>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter your e-mail address"
-              name="email"
-              value={email}
-              onChange={(event) => onInputChange(event)}
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-orange mx-2">
-            Cancel
-          </button>
+          <form onSubmit={(event) => onSubmit(event)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(event) => onInputChange(event)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your username"
+                name="username"
+                value={username}
+                onChange={(event) => onInputChange(event)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your e-mail address"
+                name="email"
+                value={email}
+                onChange={(event) => onInputChange(event)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-orange mx-2" to="/">
+              Cancel
+            </Link>
+          </form>
         </div>
       </div>
     </div>
